fe4d3f32fc38cf0219993581114e04a1c8cbb1b3
Hafizh Arrizqa <fakeriz.id@gmail.com>
2022-04-18 11:10:40 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Zero Kernel by Fakeriz.
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=surya
+device.name2=karna
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
